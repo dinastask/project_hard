a65f08d10f699bdb1b88ad8d35a4cf00e4d8bc91
dinastask <89831960018pd@mail.ru>
2021-02-13 00:33:06 +0700

    end update media

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-
+  //настройки акардиона на section main
   var mainListBlock = $(".main-list-block");
   var trendingShoppingItem = $(".main-content-list-item");
 
@@ -14,31 +14,39 @@ $(document).ready(function () {
     console.log($(acctiveContent));
   });
 
+  //настройки всплывающего меню
   var menuButton = $(".header-menu");
   menuButton.on("click", function () {
     $(".navbar-header").toggleClass("navbar-header--visible");
   });
 
+  //настройки слайдера на section main
   const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
   
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
   });
+  
+
+  $(document).ready( function() {
+    /* Check width on page load*/
+    if ( $(window).width() < 768) {
+     $('.main-content-list-item').removeClass('main-content-list-item--active');
+    }
+    else {
+      
+    }
+
+
+});
+
+
+
+
   
 });
